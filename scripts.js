4b4aa9562e261b3fe9dadf8ac8d1bba4336a8c9c
Anna Sedlar <annasedlar@gmail.com>
2016-12-19 11:14:26 -0500

    working on json

--- scripts.js
+++ scripts.js
@@ -1,9 +1,17 @@
 $(document).ready(function(){
 	$('.yahoo-form').submit(function(){
-		//stop the form from submitting (default action)
+		//stop the form from submitting (default action) - will stay in console
 		event.preventDefault(); 
+		// get whatever the user typed out of the input and store it in 
 		var symbol = $('#symbol').val();
-		console.log(symbol);
+		//dynamically build the URL to use the symbols the user requested
+ 		var url= "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20('"+symbol+"')%0A%09%09&env=http%3A%2F%2Fdatatables.org%2Falltables.env&format=json";
+ 		// console.log(url);
+ 		//getjson(where to go, what to do)
+ 		$.getJSON(url, function(data){
+ 			var stockInfo = data.query.results.quote;
+ 		});
+ 		console.log("where is JS?");
 
 	});
 });
