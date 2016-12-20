f8dd4b339c1041936649c499ec8feb12e286beb1
Anna Sedlar <annasedlar@gmail.com>
2016-12-20 09:20:58 -0500

    fixed jsonData function to include url

--- scripts.js
+++ scripts.js
@@ -1,28 +1,35 @@
 $(document).ready(function(){
 
+
+	var searchedStocks = [];
 	// See if the user has any stored stocks. If so, then load them
 	var userStocksSaved = localStorage.getItem('userStocks');
-	// we ran split on userStocks localstorage var and it became an array!/
-	// for(let i = 0; i < userStocksSaved.length; i++){
-	// 	var htmlToPlot = buildStockRow(userStocksSaved[i]);
-	// 	$('#stock-body').append(htmlToPlot);
-	// }
-
-	var url = `http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20("${userStocksSaved}")%0A%09%09&env=http%3A%2F%2Fdatatables.org%2Falltables.env&format=json`;
-	newest(url);
+	// var url = `http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20("${userStocksSaved}")%0A%09%09&env=http%3A%2F%2Fdatatables.org%2Falltables.env&format=json`;
+	jsonData(userStocksSaved);
 
 	$('.yahoo-form').submit(function(){
 		// Stop the form from submitting (default action)
 		event.preventDefault();
 		// Get whatever the user typed out of the input and store it in symbol
 		var symbol = $('#symbol').val();
-		localStorage.setItem("userStocks", symbol);
+		searchedStocks.push(symbol);
 
 		// Dynamically build the URL to use the symbol(s) the user requested
-		var url = `http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20("${symbol}")%0A%09%09&env=http%3A%2F%2Fdatatables.org%2Falltables.env&format=json`;
+		// var url = `http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20("${symbol}")%0A%09%09&env=http%3A%2F%2Fdatatables.org%2Falltables.env&format=json`;
 		// getJSON, param1 = where to go, param2 = what to do
-		newest(url)
+		jsonData(symbol)
 	});
+
+	$('.btn-info').click(function(){ 
+		localStorage.setItem("userStocks", searchedStocks);
+		console.log(searchedStocks);
+
+	})
+
+	$('.btn-danger').click(function(){ 
+		localStorage.setItem("userStocks", '');
+
+	})
 });
 
 function buildStockRow(stock){
@@ -47,18 +54,19 @@ function buildStockRow(stock){
 	return newHTML;
 }
 
-function newest(url){
+function jsonData(symbol){
+	var url = `http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20("${symbol}")%0A%09%09&env=http%3A%2F%2Fdatatables.org%2Falltables.env&format=json`;
 	$.getJSON(url, function(dataJSGotIfAny){
 	var stockInfo = dataJSGotIfAny.query.results.quote;
 	if(dataJSGotIfAny.query.count == 1){
 		// we know this is a single object becaues theres only 1
 		var htmlToPlot = buildStockRow(stockInfo);
-		$('#stock-body').append(htmlToPlot);				
+		$('#stock-body-1').append(htmlToPlot);				
 	}else{
 		// we know this is an array, because the count isnt 1
 		for(let i = 0; i < stockInfo.length; i++){
 			var htmlToPlot = buildStockRow(stockInfo[i]);
-			$('#stock-body').append(htmlToPlot);
+			$('#stock-body-1').append(htmlToPlot);
 		}
 	}
 	});
